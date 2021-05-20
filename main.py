14f61c9f917f54d44d59bd28f16b9e590a255f64
Jonathan Adithya <jo.adithya391@gmail.com>
2021-05-20 15:20:08 +0700

    send message to desired phone number with twilio

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from dotenv import load_dotenv
 import requests
 import os
+from twilio.rest import Client
 
 load_dotenv()
+account_sid = os.environ['TWILIO_ACCOUNT_SID']
+auth_token = os.environ['TWILIO_AUTH_TOKEN']
 url = 'https://api.openweathermap.org/data/2.5/onecall'
 parameter = {
     'lat': -6.238960,
@@ -17,5 +20,11 @@ weather_data = response.json()['hourly'][:13]
 weather_data = filter(lambda x: x['weather'][0]['id'] < 700, weather_data)
 
 for _ in weather_data:
-    print('it is going to rain')
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(
+        body="It's going to rain today. Remember to bring an ☔",
+        from_=os.environ.get('VIRTUAL_PHONE'),
+        to=os.environ.get('PHONE_NUMBER')
+    )
+
     break
